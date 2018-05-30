763a450881705ce8ba33d3745ff97a9a250d0d15
xb199143 <p000fguimaraes@prservicos.com.br>
2018-05-30 10:08:05 -0300

    primeiro commit

---
+++ src/main/java/br/com/devmedia/domain/Playlist.java
@@ -0,0 +1,69 @@
+package br.com.devmedia.domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name="playlist")
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Size(min = 2, max = 60)
+    @Column(nullable = false, length = 60)
+    private String nome;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String descricao;
+
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL)
+    private List<Musica> musicas;
+
+    public Playlist() {
+    }
+
+    public Playlist(String nome, String descricao, List<Musica> musicas) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.musicas = musicas;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+}
